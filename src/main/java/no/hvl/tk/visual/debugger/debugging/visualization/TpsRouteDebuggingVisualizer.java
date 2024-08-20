71316957db6478acc70087dec8f56f96b41bafaf
Mahmoud Sallam <mahmoud.sallam@teampicnic.com>
2024-08-20 20:43:57 +0200

    (wip) start a workable solution, add a sample route and a script to visualize a singular route

--- src/main/java/no/hvl/tk/visual/debugger/debugging/visualization/TpsRouteDebuggingVisualizer.java
+++ src/main/java/no/hvl/tk/visual/debugger/debugging/visualization/TpsRouteDebuggingVisualizer.java
@@ -3,25 +3,23 @@ package no.hvl.tk.visual.debugger.debugging.visualization;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.ui.components.JBScrollPane;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import net.sourceforge.plantuml.FileFormat;
 import net.sourceforge.plantuml.FileFormatOption;
 import net.sourceforge.plantuml.SourceStringReader;
 import no.hvl.tk.visual.debugger.SharedState;
-import no.hvl.tk.visual.debugger.domain.ODAttributeValue;
-import no.hvl.tk.visual.debugger.domain.ODLink;
-import no.hvl.tk.visual.debugger.domain.ODObject;
-import no.hvl.tk.visual.debugger.domain.ObjectDiagram;
 import no.hvl.tk.visual.debugger.ui.CopyPlantUMLDialog;
+import org.jetbrains.annotations.NotNull;
 
 public class TpsRouteDebuggingVisualizer extends DebuggingInfoVisualizerBase {
     private static final Logger LOGGER = Logger.getInstance(TpsRouteDebuggingVisualizer.class);
@@ -39,7 +37,34 @@ public class TpsRouteDebuggingVisualizer extends DebuggingInfoVisualizerBase {
 
     @Override
     public void visualizeFurther(TpsDebugData route) {
-        // todo: process the tps objects here
+        // Create a Python interpreter
+
+
+//        String[] command = {"pipenv", "install", "matlib", "&&", "pipenv", "run", "python3", "single_route_visualizer.py"};
+
+
+        try {
+            // Extract Python script from resources to a temporary file
+            InputStream pythonScript = TpsRouteDebuggingVisualizer.class.getClassLoader().getResourceAsStream("scripts/single_route_visualizer.py");
+            InputStream sample = TpsRouteDebuggingVisualizer.class.getClassLoader().getResourceAsStream("route_model_from_algorithm_sample.json");
+            if (pythonScript != null) {
+                File scriptFile = copyToTempFile(pythonScript, "script", ".py");
+                File sampleFile = copyToTempFile(sample, "sample", ".json");
+
+                ProcessBuilder processBuilder = new ProcessBuilder("pipenv", "install", "matplotlib");
+                ProcessBuilder processBuilder2 = new ProcessBuilder("pipenv", "run", "python3", scriptFile.getAbsolutePath(), "--file_path", sampleFile.getAbsolutePath());
+                runPythonTask(processBuilder);
+                runPythonTask(processBuilder2);
+
+            } else {
+                System.out.println("files not found in resources");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        // todo: process the tps objects here (write logic here)
         // aka:
         // call a python script, and build the image here, that should be it.
         final var routeString = route.toString();
@@ -47,9 +72,44 @@ public class TpsRouteDebuggingVisualizer extends DebuggingInfoVisualizerBase {
         try {
             final byte[] pngData = toImage(routeString, FileFormat.PNG);
             this.addImageToUI(pngData);
-        } catch (final IOException e) {
+        } catch (
+                final IOException e) {
             LOGGER.error(e);
         }
+    }
+
+    private static File copyToTempFile(InputStream inputStream, String fileName, String fileType) throws IOException {
+        File tempFile = File.createTempFile(fileName, fileType);
+        Files.copy(inputStream, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        return tempFile;
+    }
+
+    private static void runPythonTask(ProcessBuilder processBuilder) throws IOException, InterruptedException {
+        Process process = processBuilder.start();
+        // Capture the output if needed
+        process.waitFor();
+        System.out.println();
+
+
+        // Read standard output from the script (stdout)
+        InputStream stdout = process.getInputStream();
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(stdout));
+
+        // Read standard error from the script (stderr)
+        InputStream stderr = process.getErrorStream();
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(stderr));
+
+        String s;
+        System.out.println("Standard Output:");
+        while ((s = stdInput.readLine()) != null) {
+            System.out.println(s);
+        }
+
+        System.out.println("Error Output (if any):");
+        while ((s = stdError.readLine()) != null) {
+            System.out.println(s);
+        }
+        System.out.println();
     }
 
 
---
+++ src/main/resources/scripts/single_route_visualizer.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import json
+import argparse
+
+
+def convert_ms_to_datetime(ms):
+    # return datetime.datetime.fromtimestamp(ms / 1000.0)
+    return ms / 1000
+
+
+def main(file_path):
+    with open(file_path) as route_json:
+        route = json.load(route_json)
+    nodes = route['nodes']
+    activities = [node['node_type'] for node in nodes]
+
+    start_times = [convert_ms_to_datetime(node['service_start_time']) for node in nodes]
+    durations = [node["adjusted_service_duration"] / 1000.0 for node in nodes]
+
+    plt.figure(figsize=(10, 6))
+
+    plt.barh(activities, durations, left=start_times, color='skyblue')
+
+    # # add labels
+    # for i, (start, duration) in enumerate(zip(start_times, durations)):
+    #     plt.text(start + duration / 2, i, activities[i],
+    #              ha='center', va='center', color='black')
+
+    plt.title('Route Timeline')
+    plt.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Plot route timeline from a json file")
+    parser.add_argument('--file_path', type=str, help='Path to the sample file')
+
+    args = parser.parse_args()
+
+    main(args.file_path)
